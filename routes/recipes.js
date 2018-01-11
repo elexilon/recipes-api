6bddd95210dfc2ddf3fd3ae4e87b1b33604b918c
ELEXILON <elexilon@gmail.com>
2018-01-11 13:18:45 +0100

    create update and delete routes

--- routes/recipes.js
+++ routes/recipes.js
@@ -27,25 +27,57 @@ router.get('/recipes', (req, res, next) => {
       .catch((error) => next(error))
   })
   .put('/recipes/:id', (req, res, next) => {
-    // let updaRecipe = req.body
-    //
-    // Recipe.findById(updaRecipe.id)
-    //   .then((recipe) => {
-    //     if (!recipe) { return next() }
-    //     res.json(updaRecipe)
-    //   })
-    //   .catch((error) => next(error))
+    let updaRecipe = req.body
+
+    Recipe.findById({id: updaRecipe.id}, updaRecipe);
+      .then((recipe) => {
+        if (!recipe) { return next() }
+        recipe.title        = updaRecipe.title        || recipe.title
+        recipe.summary      = updaRecipe.summary      || recipe.summary
+        recipe.photo        = updaRecipe.photo        || recipe.photo
+        recipe.vegan        = updaRecipe.vegan        || recipe.vegan
+        recipe.vegetarian   = updaRecipe.vegetarian   || recipe.vegetarian
+        recipe.pescatarian  = updaRecipe.pescatarian  || recipe.pescatarian
+        recipe.cookingTime  = updaRecipe.cookingTime  || recipe.cookingTime
+        recipe.ingredients  = updaRecipe.ingredients  || recipe.ingredients
+        recipe.cookingSteps = updaRecipe.cookingSteps || recipe.cookingSteps
+        recipe.likedBy      = updaRecipe.likedBy      || recipe.likedBy
+        recipe.authorId     = updaRecipe.authorId     || recipe.authorId
+        recipe.createdAt    = updaRecipe.createdAt    || recipe.createdAt
+        recipe.updatedAt    = updaRecipe.updatedAt    || recipe.updatedAt
+        recipe.save(recipe)
+        res.json(recipe)
+      })
+      .catch((error) => next(error))
   })
   .patch('/recipes/:id', (req, res, next) => {
+    let updaRecipe = req.body
+
+    Recipe.findById({id: updaRecipe.id}, updaRecipe);
+      .then((recipe) => {
+        if (!recipe) { return next() }
+        recipe.title        = updaRecipe.title        || recipe.title
+        recipe.summary      = updaRecipe.summary      || recipe.summary
+        recipe.photo        = updaRecipe.photo        || recipe.photo
+        recipe.vegan        = updaRecipe.vegan        || recipe.vegan
+        recipe.vegetarian   = updaRecipe.vegetarian   || recipe.vegetarian
+        recipe.pescatarian  = updaRecipe.pescatarian  || recipe.pescatarian
+        recipe.cookingTime  = updaRecipe.cookingTime  || recipe.cookingTime
+        recipe.ingredients  = updaRecipe.ingredients  || recipe.ingredients
+        recipe.cookingSteps = updaRecipe.cookingSteps || recipe.cookingSteps
+        recipe.likedBy      = updaRecipe.likedBy      || recipe.likedBy
+        recipe.authorId     = updaRecipe.authorId     || recipe.authorId
+        recipe.createdAt    = updaRecipe.createdAt    || recipe.createdAt
+        recipe.updatedAt    = updaRecipe.updatedAt    || recipe.updatedAt
+        recipe.save(recipe)
+        res.json(recipe)
+      })
+      .catch((error) => next(error))
 
   })
   .delete('/recipes/:id', (req, res, next) => {
     const id = req.params.id
-    Recipe.findById(id)
-      .then((recipe) => {
-        if (!recipe) { return next() }
-        recipe.destroy()
-      })
+    Recipe.findByIdAndRemove(id)
       .catch((error) => next(error))
   })
 
